f9ff9d97b4fab732762feefb46864e9748c01aa6
Jaewon <dhwodnjs0827@naver.com>
2025-02-18 19:34:53 +0900

    [ Feat ] 플레이어 이동 및 회전 구현
    ++ InputSystem을 이용한 플레이어 입력 처리
    
    ++ 캐릭터를 컨트롤할 BaseConroller.cs 추가
    
    ++ 플레이어를 컨트롤할 PlayerController.cs 추가

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    #region Variables
+    protected Rigidbody2D rb;
+    protected BoxCollider2D boxCol;
+    protected SpriteRenderer characterRenderer;
+
+    protected Vector2 moveDir;
+    protected Vector2 lookDir;
+
+    protected float moveSpeed;
+    #endregion
+
+    #region Unity Event Method
+    protected virtual void Awake()
+    {
+        InitializeComponent();
+        InitializeVariable();
+    }
+
+    protected virtual void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        Rotate(lookDir);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Move(moveDir);
+    }
+    #endregion
+
+    /// <summary>
+    /// 캐릭터의 행동을 처리하는 메서드
+    /// </summary>
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    /// <summary>
+    /// 이동 방향과 이동 속도를 곱한 값을 캐릭터의 속도로 설정
+    /// </summary>
+    /// <param name="dir">캐릭터의 이동 방향</param>
+    private void Move(Vector2 dir)
+    {
+        dir = dir * moveSpeed;
+        rb.velocity = dir;
+    }
+
+    /// <summary>
+    /// 바라보는 좌표의 x값과 y값 사이의 각도를 구해 오른쪽을 바라보면 오른쪽을, 왼쪽을 바라보면 왼쪽으로 캐릭터를 뒤집는다.
+    /// </summary>
+    /// <param name="dir">바라보는 좌표 벡터</param>
+    private void Rotate(Vector2 dir)
+    {
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        characterRenderer.flipX = Mathf.Abs(angle) > 90f;
+    }
+
+    /// <summary>
+    /// 컴포넌트를 초기화 시켜주는 메서드
+    /// </summary>
+    protected virtual void InitializeComponent()
+    {
+        if (!TryGetComponent<Rigidbody2D>(out rb))
+        {
+            rb = gameObject.AddComponent<Rigidbody2D>();
+            rb.gravityScale = 0f;
+        }
+
+        characterRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// 변수를 초기화 시켜주는 메서드
+    /// </summary>
+    protected virtual void InitializeVariable()
+    {
+        moveDir = Vector2.zero;
+        lookDir = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    private Camera mainCam;
+
+    #region Unity Event Method
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        mainCam = Camera.main;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+    #endregion
+
+    protected override void InitializeComponent()
+    {
+        base.InitializeComponent();
+
+        if (!TryGetComponent<BoxCollider2D>(out boxCol))
+        {
+            boxCol = gameObject.AddComponent<BoxCollider2D>();
+            boxCol.offset = new Vector2(0, -0.2148886f);
+            boxCol.size = new Vector2(1, 1.429777f);
+        }
+    }
+
+    protected override void InitializeVariable()
+    {
+        base.InitializeVariable();
+        moveSpeed = 5f;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        moveDir = inputValue.Get<Vector2>();
+        moveDir.Normalize();
+    }
+
+    private void OnLook(InputValue inputValue)
+    {
+        Vector2 mousePos = inputValue.Get<Vector2>();
+        mousePos = mainCam.ScreenToWorldPoint(mousePos);
+        lookDir = mousePos - (Vector2)transform.position;
+
+        if (lookDir.magnitude < 0.9f)
+        {
+            lookDir = Vector2.zero;
+        }
+        else
+        {
+            lookDir.Normalize();
+        }
+    }
+}
